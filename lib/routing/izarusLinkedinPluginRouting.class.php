0f0d818613d47b73f86449395644211bf4149ffc
David Vega <david@izarus.cl>
2014-09-03 16:55:26 -0400

    Autenticacion y conexion

--- lib/routing/izarusLinkedinPluginRouting.class.php
+++ lib/routing/izarusLinkedinPluginRouting.class.php
@@ -7,6 +7,7 @@ class izarusLinkedinPluginRouting
     $event->getSubject()->prependRoute('linkedin_signin', new sfRoute('/linkedin/login', array('module' => 'sfLinkedinAuth', 'action' => 'signin')));
     $event->getSubject()->prependRoute('linkedin_signin_response', new sfRoute('/linkedin/response', array('module' => 'sfLinkedinAuth', 'action' => 'signinResponse')));
     $event->getSubject()->prependRoute('linkedin_access_denied', new sfRoute('/linkedin/access-denied', array('module' => 'sfLinkedinAuth', 'action' => 'accessDenied')));
+    $event->getSubject()->prependRoute('linkedin_connect', new sfRoute('/linkedin/connect', array('module' => 'sfLinkedinAuth', 'action' => 'connect')));
   }
 }
 
--- modules/sfLinkedinAuth/lib/BasesfLinkedinAuthActions.class.php
+++ modules/sfLinkedinAuth/lib/BasesfLinkedinAuthActions.class.php
@@ -26,7 +26,24 @@ class BasesfLinkedinAuthActions extends sfActions
 
     $token = $linkedin->getAccessToken($request->getParameter('code'));
 
-    $user_data = $linkedin->get('/people/~:(id,first-name,last-name,email-address)')
+    $user_data = $linkedin->get('/people/~:(id,first-name,last-name,email-address)');
+
+
+    if ($this->getUser()->getFlash('is_linkedin_connect')) {
+      // Busca si la cuenta LinkedIn está asociada a otro usuario
+      $otro_usuario = sfGuardUserTable::getInstance()->findOneByLinkedinUid($user_data['id']);
+      if ($otro_usuario && $otro_usuario->getId() != $this->getUser()->getGuardUser()->getId()) {
+        throw new Exception("Otro usuario tiene asignada esta cuenta LinkedIn");
+      }
+
+      $usuario = $this->getUser()->getGuardUser();
+      $usuario->set(sfConfig::get('app_linkedin_guard_uid_column','linkedin_uid'),$user_data['id']);
+      $usuario->set(sfConfig::get('app_linkedin_guard_token_column','linkedin_token'),$token);
+      $usuario->save();
+
+      $this->redirect(sfConfig::get('app_linkedin_after_connect_url','@homepage'));
+    }
+
 
     // Busca usuario ya logeado previo con linkedin
     $usuario = sfGuardUserTable::getInstance()->findOneByEmailAddress($user_data['emailAddress']);
@@ -51,6 +68,24 @@ class BasesfLinkedinAuthActions extends sfActions
 
   public function executeAccessDenied(sfWebRequest $request)
   {
+
+  }
+
+  public function executeConnect(sfWebRequest $request)
+  {
+    $this->forward404Unless($this->getUser()->isAuthenticated());
+
+    $linkedin = new sfLinkedin($this->getContext()->getRouting()->generate('linkedin_signin_response',array(),true));
+
+    $url = $linkedin->getLoginUrl(array(
+      'r_fullprofile',
+      'r_emailaddress',
+      'r_contactinfo',
+      ));
+
+    $this->getUser()->setFlash('is_linkedin_connect',true);
+
+    $this->redirect($url);
 
   }
 
